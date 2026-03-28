cb56dedb3bde8a1bef30e808ea931626b58b2ff0
Setoti <78144526+Setoti@users.noreply.github.com>
2026-03-28 16:44:14 +0000

    update app.py

--- app.py
+++ app.py
@@ -14,23 +14,23 @@ st.title("どうぶつ語ボイスチェンジャー")
 st.write("音声をアップロードして、どうぶつ語に加工しましょう。")
 
 # 重い処理をキャッシュする関数
-#@st.cache_data
-#def animal_voice_effect( x, fs, speed, f0_base):
+@st.cache_data
+def animal_voice_effect( x, fs, speed, f0_base):
     # 基本周波数、スペクトル包絡、非周期指標の抽出
-    #_f0, t = pw.dio(x, fs)  # 基本周波数の抽出
-    #f0 = pw.stonemask(x, _f0, t, fs)  # 基本周波数の修正
-    #sp = pw.cheaptrick(x, f0, t, fs)  # スペクトル包絡の抽出
-    #ap = pw.d4c(x, f0, t, fs)  # 非周期性指標の抽出
+    _f0, t = pw.dio(x, fs)  # 基本周波数の抽出
+    f0 = pw.stonemask(x, _f0, t, fs)  # 基本周波数の修正
+    sp = pw.cheaptrick(x, f0, t, fs)  # スペクトル包絡の抽出
+    ap = pw.d4c(x, f0, t, fs)  # 非周期性指標の抽出
 
     # 基本周波数の変化の抑制
-    #f0[f0>0] = (f0[f0>0] - f0_base) * 0.1 + f0_base
-    #synthesized = pw.synthesize(f0, sp, ap, fs)
+    f0[f0>0] = (f0[f0>0] - f0_base) * 0.1 + f0_base
+    synthesized = pw.synthesize(f0, sp, ap, fs)
 
     ## 早送り再生（リサンプリング）
-    #fs_new = int(fs/speed)
-    #y = resampy.resample(synthesized, sr_orig=fs, sr_new=fs_new)
+    fs_new = int(fs/speed)
+    y = resampy.resample(synthesized, sr_orig=fs, sr_new=fs_new)
 
-    #return y
+    return y
 
 uploaded_file = st.file_uploader("音声ファイルを選択", type=['mp3', 'wav', 'ogg'])
 
@@ -57,7 +57,7 @@ if uploaded_file is not None:
     # 実行ボタン（スライダーを動かしただけでは重い処理をさせない）
     with st.spinner("エフェクトを計算中..."):
         # エフェクト処理
-        y_processed = y #animal_voice_effect( y, sr, speed, f0 )
+        y_processed = animal_voice_effect( y, sr, speed, f0 )
 
         # プレビュー再生
         preview_io = io.BytesIO()
